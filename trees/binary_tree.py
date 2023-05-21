bd4f9486663fa1fa17fa56ccde046e5525b258d9
abdulkareemAbunabhan <zman17881@gmail.com>
2023-05-21 06:15:50 +0000

    halfway

--- trees/binary_tree.py
+++ trees/binary_tree.py
@@ -6,7 +6,7 @@ class Binary_trees:
         self.res = []
         self.temp=None
 
-    def add_leef(self,value):
+    def add(self,value):
         node=Node(value)
         if(not self.root):
             self.root=node
@@ -21,28 +21,100 @@ class Binary_trees:
                 return self.res.append(value)
             else:
                 self.temp=self.temp.left
-                self.add_leef(value)    
+                self.add(value)    
         
-    def pre_order(self,arg=None,resu=[]):
+    def pre_order(self,arg=None,resu=None):
+        if(resu is None):
+            resu=[]
         if (arg==None):
             arg=self.root
-            self.pre_order(arg)
+            self.pre_order(arg,resu)
             return resu
         if(arg):
             resu.append(arg.value)
             if(arg.left):
-               self.pre_order(arg.left)
+               self.pre_order(arg.left,resu)
             if(arg.right):
-                self.pre_order(arg.right)
+                self.pre_order(arg.right,resu)
 
-    def in_order(self,arg=None,resu=[]):
+    def in_order(self,arg=None,resu=None):
+        if(resu is None):
+            resu = []
         if(arg==None):
             arg=self.root
         if(arg):
             if(arg.left):
-                self.pre_order(arg.left,resu)
+                self.in_order(arg.left,resu)
             resu.append(arg.value)
             if(arg.right):
-                self.pre_order(arg.right,resu)
+                self.in_order(arg.right,resu)
         if(arg == self.root):
             return resu
+    
+    def post_order(self,arg=None,resu=None):
+        if(arg is None):
+            arg = self.root
+        if(resu is None):
+            resu = []
+        if(arg):
+            if(arg.left):
+                self.post_order(arg.left,resu)
+            if(arg.right):
+                self.post_order(arg.right,resu)
+            resu.append(arg.value)
+        if(arg is self.root):
+            return resu
+
+class Binary_search_tree(Binary_trees):
+    def __init__(self):
+        super().__init__()
+
+    def adds(self,value,temp=None):
+        node=Node(value)
+        if(not self.root):
+            self.root=node
+            return
+        if(temp==None):
+            temp=self.root
+        if(value>temp.value):
+            if(temp.right):
+                self.adds(value,temp.right)
+            else:
+                temp.right=node
+                return
+        elif(value<temp.value):
+            if(temp.left):
+                self.adds(value,temp.left)
+            else:
+                temp.left=node
+                return
+        else:
+            return
+
+    def contains(self,value,temp=None):
+        if(not self.root):
+            return "empty tree"
+        if(temp == None):
+            temp = self.root
+        if(temp.value==value):
+            return True
+        elif(temp.value<value):
+            if(temp.left):
+             self.contains(value,temp.left)
+            else:
+                return False
+        else:
+            if(temp.right):
+             self.contains(value,temp.right)
+            else:
+                return False
+        return False
+    
+    def pre_order(self, arg=None, resu=None):
+        return super().pre_order(arg, resu)
+    
+    def in_order(self, arg=None, resu=None):
+        return super().in_order(arg, resu)
+    
+    def post_order(self, arg=None, resu=None):
+        return super().post_order(arg, resu)
