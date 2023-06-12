00df444a68ee8a53e46e479175c527bb71ff583c
abdulkareemAbunabhan <zman17881@gmail.com>
2023-06-12 17:25:34 +0000

    added doc strings

--- insertation_sort/insertation.py
+++ insertation_sort/insertation.py
@@ -1,4 +1,6 @@
 def insert(sorted,value):
+    """this function will take a sorted array and a number to insert in it as an arguments and return the array after
+    inserting the number in  the right place"""
     i=0
     while i<len(sorted) and value > sorted[i]:
         i+=1
@@ -10,6 +12,8 @@ def insert(sorted,value):
     sorted.append(value)
 
 def insertionSort(list):
+  """this function takes a list as an argument and iterate on each element and send each element to other function
+  to return a new sorted array"""
   sorted = []
   sorted.append(list[0])
   for i in range(1,(len(list))):
