f6a7213ec8542b46646f0fd970356ba0dd2ee180
abdulkareemAbunabhan <zman17881@gmail.com>
2023-07-17 20:55:34 +0000

    starter code

--- hashmap-left-join/hashmap_left_join.py
+++ hashmap-left-join/hashmap_left_join.py
@@ -1,12 +1,26 @@
+from hasher import *
 def left_join(first_hashmap,second_hashmap):
     """this function is responsible on doing left join on two hash tables and return the result is list of lists"""
     result=[]
-    if first_hashmap == {}:
+    if first_hashmap.map == []:
         return result
     else:
-        for i in first_hashmap:
-            if i in second_hashmap:
-                result.append([i,first_hashmap[i],second_hashmap[i]])
+        for i in first_hashmap.map:
+            result.append("s")
+            if i is None:
+                result.append("a")
+                pass
+            elif isinstance(i,Linked_list):
+                result.append("d")
+                current=i.head
+                while current:
+                    if second_hashmap.HasKey(current.value):
+                        result.append[current.value,first_hashmap.GetKeyValue(current.value),second_hashmap.GetKeyValue(current.value)]
+                    current=current.next
+            elif second_hashmap.HasKey(i[0]):
+                result.append("s")
+                result.append([i[0],first_hashmap.GetKeyValue(i[0]),second_hashmap.GetKeyValue(i[0])])
             else:
-                result.append([i,first_hashmap[i],None])
+                result.append("e")
+                result.append([i[0],first_hashmap.GetKeyValue(i[0]),None])
         return result
--- hashmap-left-join/tests/test_hashmap_left_join.py
+++ hashmap-left-join/tests/test_hashmap_left_join.py
@@ -1,28 +1,35 @@
 import pytest
+from hasher import *
 from hashmap_left_join import left_join
 
 def test_left_join():
     # Test case 1: Both hash maps have common keys (with synonyms and antonyms)
-    synonyms = {'happy': 'joyful', 'big': 'large', 'small': 'tiny'}
-    antonyms = {'happy': 'sad', 'big': 'small', 'small': 'big'}
+    synonyms=HashTable()
+    antonyms=HashTable()
+    synonyms.Set('happy','joyful')
+    synonyms.Set('big','large')
+    synonyms.Set('small','tiny')
+    antonyms.Set('happy','sad')
+    antonyms.Set('big','small')
+    antonyms.Set('small','big')
     assert left_join(synonyms, antonyms) == [['happy', 'joyful', 'sad'], ['big', 'large', 'small'], ['small', 'tiny', 'big']]
 
     # Test case 2: Only one key is common (with synonyms and antonyms)
-    synonyms = {'happy': 'joyful', 'big': 'large', 'small': 'tiny'}
-    antonyms = {'good': 'bad', 'big': 'small', 'small': 'big'}
-    assert left_join(synonyms, antonyms) == [['happy', 'joyful', None], ['big', 'large', 'small'], ['small', 'tiny', 'big']]
+    # synonyms = {'happy': 'joyful', 'big': 'large', 'small': 'tiny'}
+    # antonyms = {'good': 'bad', 'big': 'small', 'small': 'big'}
+    # assert left_join(synonyms, antonyms) == [['happy', 'joyful', None], ['big', 'large', 'small'], ['small', 'tiny', 'big']]
 
-    # Test case 3: No common keys (with synonyms and antonyms)
-    synonyms = {'happy': 'joyful', 'big': 'large', 'small': 'tiny'}
-    antonyms = {'good': 'bad', 'fast': 'slow', 'hot': 'cold'}
-    assert left_join(synonyms, antonyms) == [['happy', 'joyful', None], ['big', 'large', None], ['small', 'tiny', None]]
+    # # Test case 3: No common keys (with synonyms and antonyms)
+    # synonyms = {'happy': 'joyful', 'big': 'large', 'small': 'tiny'}
+    # antonyms = {'good': 'bad', 'fast': 'slow', 'hot': 'cold'}
+    # assert left_join(synonyms, antonyms) == [['happy', 'joyful', None], ['big', 'large', None], ['small', 'tiny', None]]
 
-    # Test case 4: Empty synonyms hash map
-    synonyms = {}
-    antonyms = {'happy': 'sad', 'big': 'small'}
-    assert left_join(synonyms, antonyms) == []
+    # # Test case 4: Empty synonyms hash map
+    # synonyms = {}
+    # antonyms = {'happy': 'sad', 'big': 'small'}
+    # assert left_join(synonyms, antonyms) == []
 
-    # Test case 5: Empty antonyms hash map
-    synonyms = {'happy': 'joyful', 'big': 'large'}
-    antonyms = {}
-    assert left_join(synonyms, antonyms) == [['happy', 'joyful', None], ['big', 'large', None]]
+    # # Test case 5: Empty antonyms hash map
+    # synonyms = {'happy': 'joyful', 'big': 'large'}
+    # antonyms = {}
+    # assert left_join(synonyms, antonyms) == [['happy', 'joyful', None], ['big', 'large', None]]
